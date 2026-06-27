1d74ebd4fb2dd0887b56db8a61a9731add8e2fe2
anhtu0310 <tud342@gmail.com>
2026-06-27 20:16:53 +0700

    update nightly content

--- doc/conf.py
+++ doc/conf.py
@@ -6,23 +6,24 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'STEAM Development Kit Documentation!'
+copyright = '2026 Tu Dang'
+author = 'Tu Dang'
 release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser',"sphinx_design"]
 
 templates_path = ['_templates']
+
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = 'conestack'
+# html_static_path = ['_static']
